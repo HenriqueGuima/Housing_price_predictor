c98568ee3b2b86e930e25f9d698423dd3d8d545d
Henrique Guimarães <hbertoguima@gmail.com>
2024-07-26 12:15:39 +0100

    Lasso Regression implementation and hyperparameter tuning

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import numpy
 import matplotlib.pyplot as plt
 import seaborn as sns
 pandas.set_option('display.max_rows', None)
-from sklearn.model_selection import train_test_split
+from sklearn.discriminant_analysis import StandardScaler
+from sklearn.model_selection import GridSearchCV, RepeatedKFold, train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.linear_model import Lasso
@@ -43,60 +44,96 @@ d_c['Price_m2'] = d_c['Price'] / d_c['Area']
 d_c['Price_m2'] = d_c['Price_m2'].round(2)
 d_c.to_csv('data/portugal_apartments_cleaned.csv')
 
-# Data Visualization
-# Mean price per Location
-mean_price_location = d_c.groupby('Location')['Price_m2'].mean().sort_values(ascending=False)
-mean_price_location.plot(kind='bar')
-# plt.show()
+# Hot encoding
+d_c = pandas.get_dummies(d_c, columns=['Location'])
 
-# Mean price per number of rooms
-mean_price_rooms = d_c.groupby('Rooms')['Price_m2'].mean().sort_values(ascending=False)
-mean_price_rooms.plot(kind='bar')
-# plt.show()
+# # Data Visualization
+# # Mean price per Location
+# mean_price_location = d_c.groupby('Location')['Price_m2'].mean().sort_values(ascending=False)
+# mean_price_location.plot(kind='bar')
+# # plt.show()
+
+# # Mean price per number of rooms
+# mean_price_rooms = d_c.groupby('Rooms')['Price_m2'].mean().sort_values(ascending=False)
+# mean_price_rooms.plot(kind='bar')
+# # plt.show()
 
 # Histogram 
 d_c.hist(figsize=(12,12))
 # plt.show()
 
-# Convert dsc['Price_m2'] to numerical values
-d_c['Price_m2'] = pandas.to_numeric(d_c['Price_m2'], errors='coerce')
+# # Convert dsc['Price_m2'] to numerical values
+# d_c['Price_m2'] = pandas.to_numeric(d_c['Price_m2'], errors='coerce')
 
-# Change data type of the column price_m2 to float
-d_c['Price_m2'] = d_c['Price_m2'].astype(int)
+# # Change data type of the column price_m2 to float
+# d_c['Price_m2'] = d_c['Price_m2'].astype(int)
 
-# Shuffle rows
-dsc = d_c.sample(frac = 1)
+# # Shuffle rows
+# dsc = d_c.sample(frac = 1)
+
+X = d_c.drop(['Price_m2'], axis=1)
+y = d_c['Price_m2']
 
 # Split the data
-df_train, df_test = train_test_split(dsc, test_size=0.2, random_state=42)
+# df_train, df_test = train_test_split(dsc, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
 
 # Create a categorical version for the price_m2 variable
-dsc['Price_m2_cat'] = pandas.cut(dsc['Price_m2'], bins=5, labels=False)
+# dsc['Price_m2_cat'] = pandas.cut(dsc['Price_m2'], bins=5, labels=False)
 
 # Random splitting without stratification
-df_train, df_test = train_test_split(dsc, test_size=0.2, random_state=42)
+# df_train, df_test = train_test_split(dsc, test_size=0.2, random_state=42)
 
 # check the proportions of the data
-df_train_tc = df_train['Price_m2'].value_counts() / len(df_train)
-df_test_tc = df_test['Price_m2'].value_counts() / len(df_test)
+# df_train_tc = df_train['Price_m2'].value_counts() / len(df_train)
+# df_test_tc = df_test['Price_m2'].value_counts() / len(df_test)
 
-print('========== BEFORE ===========')
-print(df_train_tc)
-print(df_test_tc)
-print('=============================')
+# Standardization
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+# Hyperparamenter Tuning to find the optimal alpha value for the Lasso regression model
+alpha_tune = {
+    # 'alpha': numpy.logspace(-4, 0, 50),
+    'alpha': numpy.linspace(start=0.000005, stop=0.02, num=200),
+    # 'max_iter': [50000, 100000, 200000],
+    'tol': [1e-6, 1e-7, 1e-8]
+    }
+
+model_tuner = Lasso(fit_intercept=True, max_iter=25000)
+cross_validation = RepeatedKFold(n_splits=5, n_repeats=3, random_state=1)
+
+grid_search = GridSearchCV(model_tuner, alpha_tune, cv=cross_validation, scoring='neg_mean_squared_error', n_jobs=-1)
+grid_search.fit(X_train_scaled, y_train)
+
+best_model = grid_search.best_estimator_
+best_alpha = grid_search.best_params_
+
+# Train best model
+best_model.fit(X_train_scaled, y_train)
+
+# Prediction
+y_pred = best_model.predict(X_test_scaled)
+
+# print('========== BEFORE ===========')
+# print(df_train_tc)
+# print(df_test_tc)
+# print('=============================')
 
 # Save the data
-df_train.to_csv('data/portugal_apartments_train.csv')
-df_test.to_csv('data/portugal_apartments_test.csv')
+X_train_scaled.to_csv('data/portugal_apartments_train.csv')
+X_test_scaled.to_csv('data/portugal_apartments_test.csv')
 
 # Linear Regression
 # lr_model = LinearRegression()
 # lr_model.fit(df_train[['Area']], df_train['Price_m2'])
 
-lasso_model = Lasso(alpha=0.1)  # alpha is the regularization strength
-lasso_model.fit(df_train[['Area']], df_train['Price_m2'])
+# lasso_model = Lasso(alpha=0.1)  # alpha is the regularization strength
+# lasso_model.fit(df_train[['Area']], df_train['Price_m2'])
 
-y_pred_lasso = lasso_model.predict(df_test[['Area']])
+# y_pred_lasso = lasso_model.predict(df_test[['Area']])
 
 # y_pred = lr_model.predict(df_test[['Area']])
 # y_test = df_test['Price_m2']
@@ -110,8 +147,8 @@ y_pred_lasso = lasso_model.predict(df_test[['Area']])
 # R2: -0.43
 
 # Model's Evaluation
-mse = mean_squared_error(df_test['Price_m2'], y_pred_lasso)
-r2 = r2_score(df_test['Price_m2'], y_pred_lasso)
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
 
 print(f'MSE: {mse:.2f}')
 print(f'R2: {r2:.2f}')
